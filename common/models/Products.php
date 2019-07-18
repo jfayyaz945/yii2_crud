c9fafb4ccc51ba70d19c23de24c4b77bb0409a25
Junaid Fayyaz <omer.farooq@nxb.com.pk>
2019-07-18 17:50:03 +0500

    yii2 simple crud

---
+++ common/models/Products.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "products".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $code
+ * @property int $price
+ */
+class Products extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'products';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'code', 'price'], 'required'],
+            [['price'], 'integer'],
+            [['name', 'code'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'code' => 'Code',
+            'price' => 'Price',
+        ];
+    }
+}
